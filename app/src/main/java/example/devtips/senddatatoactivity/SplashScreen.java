e45965276ccb3f660b51f743474724baa5062d63
Lorenzo Battilocchi <Lorenzo.battilocchi@ucdconnect.ie>
2020-04-13 22:24:51 +0100

    Fix permisison issue with Jouney Planner
    Now asking permission on first launch.

--- app/src/main/java/example/devtips/senddatatoactivity/SplashScreen.java
+++ app/src/main/java/example/devtips/senddatatoactivity/SplashScreen.java
@@ -1,26 +1,51 @@
 package example.devtips.senddatatoactivity;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.content.Intent;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class SplashScreen extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_FOR_LOCATION = 98;
     Button journeyPlan;
     Button greenTips;
+
+//    NB: Required to request permissions in advance. Else, crash on first opening of GMaps
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FOR_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "This app requires Location permission. Please allow it through Settings app or restart the app to receive the dialog again..", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FOR_LOCATION);
+        }
 
         setContentView(R.layout.activity_splash_screen);
 
