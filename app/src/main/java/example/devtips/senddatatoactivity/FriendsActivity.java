cdccdc745f9f7608739b46de1e6865d266713d3a
Lorenzo Battilocchi (AS208114) <Lorenzo.battilocchi@ucdconnect.ie>
2020-05-04 15:18:10 +0100

    Added FAB to add more friends

--- app/src/main/java/example/devtips/senddatatoactivity/FriendsActivity.java
+++ app/src/main/java/example/devtips/senddatatoactivity/FriendsActivity.java
@@ -2,10 +2,14 @@ package example.devtips.senddatatoactivity;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FriendsActivity extends AppCompatActivity {
     String[] friendsArray = {"Bob Marley", "Louis Armstrong", "George Benson"};
@@ -13,6 +17,7 @@ public class FriendsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Friends using the App");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
 
@@ -21,5 +26,15 @@ public class FriendsActivity extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.friends_list);
 
         listView.setAdapter(adapter);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
