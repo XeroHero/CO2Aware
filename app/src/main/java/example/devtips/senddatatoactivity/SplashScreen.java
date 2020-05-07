4342591936609630a5c756df09045bb42fb285c5
Lorenzo Battilocchi (AS208114) <Lorenzo.battilocchi@ucdconnect.ie>
2020-05-07 12:10:34 +0100

    SplashScreen REdirect to Map activity

--- app/src/main/java/example/devtips/senddatatoactivity/SplashScreen.java
+++ app/src/main/java/example/devtips/senddatatoactivity/SplashScreen.java
@@ -1,8 +1,10 @@
 package example.devtips.senddatatoactivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,6 +13,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -91,6 +105,53 @@ Button friendList;
         });
     }
 
+    String url = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=17566ea530bc36033158dda33346ffbc2313d405";
+    ArrayList<BikeStation> bikeStationList = new ArrayList<>();
+
+    private void getData() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+
+                        BikeStation bikeStation = new BikeStation();
+                        bikeStation.setName(jsonObject.getString("name"));
+                        bikeStation.setAddress(jsonObject.getString("address"));
+                        bikeStation.setNumber(jsonObject.getInt("number"));
+                        bikeStation.setLat(jsonObject.getDouble("lat"));
+                        bikeStation.setLng(jsonObject.getDouble("lng"));
+                        bikeStation.setBanking(jsonObject.getBoolean("banking"));
+                        bikeStation.setBonus(jsonObject.getBoolean("bonus"));
+                        bikeStation.setParkingAvailable(jsonObject.getInt("available_bike_stands"));
+                        bikeStation.setBikesAvailable(jsonObject.getInt("available_bikes"));
+                        bikeStation.setStatus(jsonObject.getString("status"));
+                        bikeStation.setLastUpdate(jsonObject.getLong("last_update"));
+
+                        bikeStationList.add(bikeStation);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        progressDialog.dismiss();
+                    }
+                }
+//                adapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
