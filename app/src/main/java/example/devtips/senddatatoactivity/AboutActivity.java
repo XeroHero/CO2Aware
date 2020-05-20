04e5513650e9a9198bdcd12f73f91eb29b38b3d1
Lorenzo Battilocchi <Lorenzo.battilocchi@ucdconnect.ie>
2020-05-20 23:54:40 +0100

    Project Cleanup

--- app/src/main/java/example/devtips/senddatatoactivity/AboutActivity.java
+++ app/src/main/java/example/devtips/senddatatoactivity/AboutActivity.java
@@ -20,13 +20,13 @@ public class AboutActivity extends AppCompatActivity {
         aboutTitle.setText("About the App");
         aboutText = findViewById(R.id.about_subtitle_medium);
         aboutText.setText("This is my UCD Final Year Project submission for the Completion of my BSc Computer Science degree.\n\n" +
-                        " It uses many open-source libraries, such as the Gson library to hadnle the JSON retrieved from the " +
-                        "JCDecaux API dynamically each time.\n\n" +
+                " It uses many open-source libraries, such as the Gson library to hadnle the JSON retrieved from the " +
+                "JCDecaux API dynamically each time.\n\n" +
                 "All graphics used in this application are royalty-free open graphics from flaticon.com and freeiconspng.com.\n\n" +
-                        "All the real-time data is gathered from the JCDecaux API for public city bikes." +
-                        "The \"40 ways to go Green\" article is taken from conserve-energy-future.com\n\n" +
+                "All the real-time data is gathered from the JCDecaux API for public city bikes." +
+                "The \"40 ways to go Green\" article is taken from conserve-energy-future.com\n\n" +
                 "Layout design is using build-in Android Buttons (customised) and Google MaterialUI \n\n" +
-                        "Toast Messages hae been created with Toasty (by @GrenderG): The usual Toast, but with steroids \uD83D\uDCAA\n"
-                );
+                "Toast Messages hae been created with Toasty (by @GrenderG): The usual Toast, but with steroids \uD83D\uDCAA\n"
+        );
     }
 }
--- app/src/main/java/example/devtips/senddatatoactivity/BikeStationBrowser.java
+++ app/src/main/java/example/devtips/senddatatoactivity/BikeStationBrowser.java
@@ -1,11 +1,7 @@
 package example.devtips.senddatatoactivity;
 
 import android.Manifest;
-import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,12 +14,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.maps.DirectionsApi;
-import com.google.maps.DirectionsApiRequest;
-import com.google.maps.GeoApiContext;
-import com.google.maps.model.TravelMode;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,9 +28,6 @@ import retrofit2.Response;
 public class BikeStationBrowser extends FragmentActivity implements OnMapReadyCallback {
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     static GoogleMap mMap;
-    static String replace;
-//    MarkerOptions origin, destination;
-//    Polyline currentPolyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,98 +39,6 @@ public class BikeStationBrowser extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-//        origin = new MarkerOptions().position(getLocationFromAddress(this, ChooseDestinationActivity.origin)).title("A");
-//        destination = new MarkerOptions().position(getLocationFromAddress(this, ChooseDestinationActivity.destination)).title("B");
-//
-//        String url = getUrl(origin.getPosition(), destination.getPosition(), ChooseDestinationActivity.transportType);
-//        new FetchURL(this).execute(url, ChooseDestinationActivity.transportType);
-    }
-
-    private String getUrl(LatLng origin, LatLng destination, String transportType) {
-        String originStr = "origin=" + origin.latitude + "," + origin.longitude;
-        String destStr = "destination=" + destination.latitude + "," + destination.longitude;
-        String mode = "mode=" + transportType;
-
-        String params = originStr + "&" + destStr + "&" + mode;
-        String output = "json";
-
-        return "https://maps.googelapis.com/maps/api/directions/" + output + "?" + params + "&key=" + getString(R.string.google_maps_key);
-    }
-
-    public LatLng getLocationFromAddress(Context context, String strAddress) {
-
-        Geocoder coder = new Geocoder(context);
-        List<Address> address;
-        LatLng p1 = null;
-
-        try {
-            // May throw an IOException
-            address = coder.getFromLocationName(strAddress, 5);
-            if (address == null) {
-                return null;
-            }
-
-            Address location = address.get(0);
-            p1 = new LatLng(location.getLatitude(), location.getLongitude());
-
-        } catch (IOException ex) {
-
-            ex.printStackTrace();
-        }
-
-        return p1;
-    }
-//
-
-    private void queryDirections() {
-        GeoApiContext context = new GeoApiContext().setApiKey("YOUR_API_KEY");
-        String destination = ChooseDestinationActivity.destination;
-
-        String origin = ChooseDestinationActivity.origin;
-        LatLng originLatLng = getLocationFromAddress(this, origin);
-
-        LatLng destLatLng = getLocationFromAddress(this, destination);
-
-        DirectionsApiRequest apiRequest = DirectionsApi.newRequest(context);
-        apiRequest.origin(new com.google.maps.model.LatLng(originLatLng.latitude, originLatLng.longitude));
-        apiRequest.destination(new com.google.maps.model.LatLng(destLatLng.latitude, destLatLng.longitude));
-        apiRequest.mode(TravelMode.DRIVING); //set travelling mode driving
-//        switch (ChooseDestinationActivity.transportType) {
-//            case "bicycling":
-//                apiRequest.mode(TravelMode.BICYCLING); //set travelling mode
-//                break;
-//            case "driving":
-//                apiRequest.mode(TravelMode.DRIVING); //set travelling mode driving
-//                break;
-//            case "walking":
-//                apiRequest.mode(TravelMode.WALKING); //set travelling mode walking
-//                break;
-//            case "transit":
-//                apiRequest.mode(TravelMode.TRANSIT); //set travelling mode transit
-//                break;
-//            case "default":
-//                apiRequest.mode(TravelMode.UNKNOWN); //generic unknown travel mode
-//
-//        }
-
-//        apiRequest.mode(TravelMode.DRIVING); //set travelling mode
-
-        //noinspection unchecked
-//TODO: Complete Directions API Call
-
-//        apiRequest.setCallback(new Callback<DirectionsResult>() {
-//            @Override
-//            public void onResponse(Call<DirectionsResult> call, Response<DirectionsResult> response) {
-//                DirectionsResult routes = response.body();
-//                System.out.println("found " +routes.geocodedWaypoints.length  + " itineraries ot the dewstination");
-//                System.out.println(routes);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<DirectionsResult> call, Throwable t) {
-//
-//            }
-//        });
     }
 
 
@@ -161,7 +57,6 @@ public class BikeStationBrowser extends FragmentActivity implements OnMapReadyCa
         LatLng dublinGeneric = new LatLng(-53, 6);
         placeMarkers(dublinGeneric, mMap);
     }
-
 
     private void placeMarkers(LatLng dublinGeneric, GoogleMap mMap) {
         //addMarkers
@@ -182,51 +77,17 @@ public class BikeStationBrowser extends FragmentActivity implements OnMapReadyCa
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dublinGeneric)); //set map to initially show a view of the entire city
         mMap.setMyLocationEnabled(true);
         defaultMapSettings(mMap);
-//        mMap.setOnMarkerClickListener(marker -> {
-//            String title = marker.getTitle();
-//            replace = title.replace("Bike Station ", " ");
-//            int bikeStationId = Integer.parseInt(replace.substring(0, 4)
-//                    .replace(" ", ""));
-//            Log.d("Bike station tapped", String.valueOf(bikeStationId)); //bikeStationId is the number parameter of each bikestation in JSON
-//
-//            return true;
-//        });
-
-// Implemented through the Maps Geolocation Framework
-//        Location myLocation = mMap.getMyLocation();
-//        if (myLocation != null) {
-//            LatLng myLocationLatLng = new LatLng(myLocation.getLatitude(),
-//                    myLocation.getLongitude());
-//
-//            mMap.addMarker(new MarkerOptions().position(myLocationLatLng)
-//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-//        }
-
     }
 
     public void addMarkerInMap(GoogleMap mMap, List<BikeStation> bikeStations) {
-//        mMap.addMarker(destination);
-//        mMap.addMarker(origin);
         for (BikeStation bikeStation : bikeStations) {
             Double lat = bikeStation.getPosition().getLatitude();
             Double lng = bikeStation.getPosition().getLongitude();
             LatLng bikeStationPosition = new LatLng(lat, lng);
             mMap.addMarker(new MarkerOptions().position(bikeStationPosition)
                     .title(bikeStation.getNumber()));
-
         }
-
-
     }
-
-//    private void clusterMarkers() {
-//        mClusterManager = new ClusterManager<>(this, mMap);
-//        mMap.setOnCameraIdleListener(mClusterManager);
-//        mMap.setOnMarkerClickListener(mClusterManager);
-//        mMap.setOnInfoWindowClickListener(mClusterManager);
-//        addMarkerInMap(mMap, respone);
-//        mClusterManager.cluster();
-//    }
 
     public static void defaultMapSettings(GoogleMap googleMap) {
         googleMap.getUiSettings().setZoomControlsEnabled(false);
@@ -238,13 +99,5 @@ public class BikeStationBrowser extends FragmentActivity implements OnMapReadyCa
         googleMap.setBuildingsEnabled(true);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53.3498091, -6.2602548), 12.17F)); //center on Spire, zoom level 12.17F OK
     }
-
-//    @Override
-//    public void onTaskDone(Object... values) {
-//        if (currentPolyline != null)
-//            currentPolyline.remove();
-//        currentPolyline=mMap.addPolyline((PolylineOptions) values[0]);
-//    }
 }
-
 
